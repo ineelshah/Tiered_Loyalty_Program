43a4f9b87bdd489590cdbf419b42b7a0d5283301
Neel Shah <neelshah.2798@gmail.com>
2021-10-31 01:15:27 -0400

    Minor Changes

--- src/mainMenu/mainMenu.java
+++ src/mainMenu/mainMenu.java
@@ -2,13 +2,15 @@ package mainMenu;
 
 import java.util.Scanner;
 import login.loginPage;
+import sampleQueries.ShowQueries;
+import signUp.registrationPage;
 
 public class mainMenu {
 	
 	
-	public void display() {
+	public static void display() {
 		System.out.println("------------------------------------------------------------------");
-		System.out.println("Home Page");
+		System.out.println("*****************************Home Page*****************************");
 		System.out.println("Please select an option from the menu: ");
 		System.out.println("1. Login");
 		System.out.println("2. Sign Up");
@@ -20,25 +22,43 @@ public class mainMenu {
 	
 	public static void main(String[] args) {
 		
-		mainMenu mainMenuInstance = new mainMenu();		
-		mainMenuInstance.display();
+		display();
 		Scanner sc = new Scanner(System.in);
 		int mainInput = 0;
-		mainInput = sc.nextInt();
 		
 		while(mainInput != 4) {
+			mainInput = sc.nextInt();
+			boolean exitFlag = false;
 			switch(mainInput) {
 				case 1:
 					loginPage loginInstance = new loginPage();
-					loginInstance.displayLoginMenu();
+					loginInstance.display();
+					break;
+				case 2:
+					registrationPage registrationInstance = new registrationPage();
+//					registrationInstance.display();
+					break;
+				case 3:
+					ShowQueries showQueriesInstance = new ShowQueries();
+//					showQueriesInstance.display();
+					break;
+				default:
+					System.out.println("Exiting program.");
+					exitFlag = true;
 					break;
 			}	
 			
-			
-			mainMenuInstance.display();
-			mainInput = sc.nextInt();
-			
+			if(!exitFlag) {
+				display();
+				mainInput = sc.nextInt();
+			}			
 			
 		}
+
+		
+		
+		
+		sc.close();
+		System.out.println("Program Terminated.");
 	}
 }
