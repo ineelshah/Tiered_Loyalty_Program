f20b636faaa1f44259f411b8e29641ca271405d2
jayrshah98 <jayrajivshah98@gmail.com>
2021-11-01 23:41:41 -0400

    Added tier

---
+++ src/brand/brandLanding_pck/loyaltyProgramModules/tier/tier.java
@@ -0,0 +1,41 @@
+package brand.brandLanding_pck.loyaltyProgramModules.tier;
+import java.util.*;
+import brand.brandLanding_pck.loyaltyProgramModules.regular.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class tier {
+		public tier() {
+			
+		}
+		public void display() {
+			Scanner sc = new Scanner(System.in);
+			System.out.println("--------------------------------------------");
+			System.out.println("Tier Page");
+			System.out.println("Please select an option from the menu:");
+			System.out.println("1. Tier Setup");
+			System.out.println("2. Add Activity Types");
+			System.out.println("3. Add Reward Types");
+			System.out.println("4. Go Back");
+			int choice= sc.nextInt();
+			switch(choice)
+			{
+			case 1: 
+				tierSetup tierSetupinstance = new tierSetupinstance();
+				tierSetupinstance.display();
+			case 2:
+				activityTypes activityTypeinstance = new activityTypes();
+				activityTypeinstance.display();
+				break;
+			case 3:
+				rewardTypes rewardTypesinstance = new rewardTypes();
+				rewardTypesinstance.display();
+				break;
+			case 4: 
+				return;
+			}
+		}
+}
