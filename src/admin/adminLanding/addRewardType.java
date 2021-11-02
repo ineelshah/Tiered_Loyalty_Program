349a18bc1eff24fdac2391b95489ce2064097494
Ameya Tathavadkar <ameya1398@gmail.com>
2021-11-02 01:12:23 -0400

    Completed admin functionality

--- src/admin/adminLanding/addRewardType.java
+++ src/admin/adminLanding/addRewardType.java
@@ -1,1 +1,33 @@
 package admin.adminLanding;
+import java.util.*;
+import rewardTypepck.rewardType;
+public class addRewardType
+{
+	//done
+	public void addRewardToDb(rewardType rew)
+	{
+		//add reward to database
+	}
+	public void display()
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the reward name");
+		System.out.println("Enter the reward code");
+		String rewardName=sc.nextLine();
+		String rewardCode=sc.nextLine();
+		rewardType rew=new rewardType();
+		rew.setRewardName(rewardName);
+		rew.setRewardCode(rewardCode);
+		System.out.println("Add Reward Type");
+		System.out.println("Go Back");
+		int choice=sc.nextInt();
+		switch(choice)
+		{
+			case 1:
+				addRewardType art=new addRewardType();
+				art.addRewardToDb(rew);
+		}
+	}
+
+	
+}
