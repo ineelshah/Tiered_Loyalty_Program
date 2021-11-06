0bc75eeeedaea8f09abc78f1d2a02e1fe73432fb
Niks2903 <niks.patil.2903@gmail.com>
2021-11-06 19:09:02 -0400

    customer functions added back

--- src/user/userLanding/LoyaltyProgram.java
+++ src/user/userLanding/LoyaltyProgram.java
@@ -1,48 +1,92 @@
 package user.userLanding;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Scanner;
 
-import user.user;
+import connection.ConnectionObj;
 import user.user;
 public class LoyaltyProgram {
 	
-//	public LoyaltyProgram() {
-//		// connection object
-//	}
+	static ResultSet rs=null;
+	static Connection conn = null;
 	
-//	public void getLoyaltyProgram() {
-//		
-//	}
+	public LoyaltyProgram() {
+		conn = ConnectionObj.getConnection();
+	}
 	
-//	public void enrollInLoyaltyProgram(int programId){
-//		
-//	}
+	public void getLoyaltyProgram() {
+		
+		String query = "SELECT DISTINCT PROGRAMID FROM LOYALTYPROGRAM";
+		Statement stmt = null;
+		try {
+			stmt = conn.createStatement();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}  
+		try {
+			rs=stmt.executeQuery(query);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		try {
+			while(rs.next()) {
+				System.out.println(rs.getString("PROGRAMID"));
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void enrollInLoyaltyProgram(String programId, String walletId){
+		
+		String activityId = "0";
+		String points = "0";
+		
+		String query = "INSERT INTO WALLET_TRANSACTIONS VALUES('" + walletId + "', '" + programId + "', '" + activityId + "', '"+ null + "', '" + points + "', '" + null + "')";
+		Statement stmt = null;
+		try {
+			stmt = conn.createStatement();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}  
+		try {
+			rs=stmt.executeQuery(query);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}  
+	}
 	
 	public void display(user u) {
 		
-		
+
 		Scanner sc = new Scanner(System.in);
 		System.out.println("--------------------------------------------");
-		System.out.println("Please select id to enroll in loyalty program:");
-		
-		//fetch loyalty programs
-		//getLoyaltyProgram()
-
-		int programId = sc.nextInt();
+		System.out.println("Please select id to enroll in loyalty program:");		
+		getLoyaltyProgram();
+		String programId = sc.next();
 		System.out.println("Please select an option from the menu:");
 		System.out.println("1. Enroll in Loyalty Program");
 		System.out.println("2. Go back");
 		int choice = sc.nextInt();
-		
+		userLanding uLanding = new userLanding();
 		switch(choice)
 		{
 			case 1:
-				//add selected programId to db
-				//enrollInLoyaltyProgram(programId)
-				
+				String walletId = u.getUserId();
+				enrollInLoyaltyProgram(programId, walletId);
+				System.out.println("successfully added");
+				uLanding.display(u);	
 				break;
-			case 2:
-				userLanding uLanding = new userLanding();
+			case 2:		
 				uLanding.display(u);
 				break;
 		}
--- src/user/userLanding/Wallet.java
+++ src/user/userLanding/Wallet.java
@@ -1,19 +1,50 @@
 package user.userLanding;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Scanner;
 
+import connection.ConnectionObj;
 import user.user;
 
 public class Wallet {
 	
-//	public Wallet() {
-//		
-//	}
+	static ResultSet rs=null;
+	static Connection conn = null;
 	
-//	public void getWalletDetails(user u) {
-//		
-//	}
+	public Wallet() {
+		conn = ConnectionObj.getConnection();
+	}
 	
+	public void getWalletDetails(String walletId) {
+		
+		String query = "SELECT * FROM WALLET_TRANSACTIONS WHERE WALLETID="+"'"+walletId+"'";
+		Statement stmt = null;
+		try {
+			stmt = conn.createStatement();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}  
+		try {
+			rs=stmt.executeQuery(query);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		try {
+			System.out.println("WALLET ID | LOYALTY PROGRAM ID| ACTIVITY ID | ACTIVITY DATE | POINTS | RE RULE CODE | ACTIVITY NAME");
+			while(rs.next()) {
+				System.out.println(rs.getString("WALLETID")+" | "+rs.getString("LOYALTY_PROGRAM_ID")+" | "+rs.getString("ACTIVITY_ID")+" | "+rs.getString("ACTIVITY_DATE")+" | "+rs.getString("POINTS")+" | "+rs.getString("RE_RULE_CODE")+" | "+rs.getString("ACTIVITY_NAME"));
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
 	public void display(user u) {
 			
 			Scanner sc = new Scanner(System.in);
@@ -22,7 +53,8 @@ public class Wallet {
 			System.out.println(" ");
 			
 			// show wallet details for customer
-			// getWalletDetails(u);
+			String walletId = u.getUserId();
+			getWalletDetails(walletId);
 			
 
 			System.out.println("Please select from below menu:");
