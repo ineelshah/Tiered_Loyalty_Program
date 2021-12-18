492e995a8fcb3b0f8ae168196f11798d1594d224
Neel Shah <shahneel.super@gmail.com>
2021-12-17 21:59:16 -0500

    Add files via upload

--- src/connection/ConnectionObj.java
+++ src/connection/ConnectionObj.java
@@ -3,6 +3,8 @@ package connection;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Scanner;
 
 public class ConnectionObj {        
     static Connection con=null;
@@ -10,20 +12,39 @@ public class ConnectionObj {
     {
         if (con != null) 
         	return con;
-        return 
-        		getConnection("root", "root");
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please input username for database: ");
+        String uname = sc.next();
+        System.out.println("Please input password for database: ");
+        String pass = sc.next();
+        
+        if(uname == null || uname.isEmpty()) {
+        	uname = "";
+        }
+        if(pass == null || pass.isEmpty()) {
+        	pass = "";
+        }
+        return getConnection(uname, pass);
     }
 
     private static Connection getConnection(String user_name,String password)
     {
         try
         {
-            Class.forName("oracle.jdbc.driver.OracleDriver");
-            con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl","root","root");  
+            Class.forName("oracle.jdbc.OracleDriver");
+//            String url = "jdbc:oracle:thin:@localhost:1521:orcl";
+              String url = "";
+//            String userName = "root";
+//            String password = "root";
+            con=DriverManager.getConnection(url, user_name, password);  
         }
-        catch(Exception e)
+        catch(SQLException e)
         {
-            e.printStackTrace();
+			System.out.println("Invalid username and password found. Please input the right credentials.");
+//            e.printStackTrace();
+        } catch (Exception e) {
+			System.out.println("Oracle JDBC jar not found. Please setup the jar and try running the program.");
         }
 
         return con;        
